ee1d14b8f7890d9b9290321f8d4ac9951ab82141
Agustin <fleitasagustinezequiel@gmail.com>
2026-06-02 18:30:13 -0300

    ultimo commit

--- recuperatorio-29-5/main.py
+++ recuperatorio-29-5/main.py
@@ -7,43 +7,44 @@ def main():
 
     while True:
 
+        print("\n--- MENU ---")
         print("1. Ingresar usuario")
-        print("2. Clasificar")
+        print("2. Clasificar usuario")
         print("3. Contar caracteres")
-        print("4. Buscar carácter")
+        print("4. Buscar caracter")
         print("5. Invertir usuario")
-        print("6. Reporte")
-        print("7. Simetría")
-        print("8. Ordenar")
+        print("6. Reporte estadistico")
+        print("7. Simetria")
+        print("8. Ordenar usuario")
         print("9. Salir")
 
-        op = input("Opción: ")
+        op = input("Opcion: ")
 
         if op == "1":
             usuario = validar_usuario()
 
         elif op == "2":
-            print(clasificar_usuario(usuario))
+            print(clasificar(usuario))
 
         elif op == "3":
-            contar_caracteres(usuario)
+            contar(usuario)
 
         elif op == "4":
-            c = input("Carácter: ")
-            buscar_caracter(usuario, c)
+            c = input("Ingrese caracter: ")
+            buscar(usuario, c)
 
         elif op == "5":
-            usuario_invertido(usuario)
+            invertir(usuario)
 
         elif op == "6":
             reporte(usuario)
 
         elif op == "7":
-            es_simetrico(usuario)
+            simetrico(usuario)
 
         elif op == "8":
-            tipo = input("A (asc) / D (desc): ")
-            ordenar(usuario, tipo)
+            orden = input("A (asc) / D (desc): ")
+            ordenar(usuario, orden)
 
         elif op == "9":
             break
